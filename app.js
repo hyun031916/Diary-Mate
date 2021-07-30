012688f7b1c378680c6bcdae75b8b54ac8632e16
hyun031916 <hyun031916@gmail.com>
2021-07-30 14:26:31 +0900

    세션 사용하여 로그인 기능 구현하기

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mysql = require('mysql');
 const format = require('date-format');
 const moment = require('moment');
+const session=require('express-session');
 const multer = require('multer');
 require('moment-timezone');
 moment.tz.setDefault('Asia/Seoul');
@@ -16,14 +17,23 @@ var prompt = new Prompt({
 const app = express();
 
 const config = require('./db/dbconn');
+const { Cookie } = require('express-session');
 
 const conn = mysql.createConnection(config);
 conn.connect();
 
+
+app.use(session({
+    secret:'keyboard cat',
+    resave:false, //사용자 접속할 때마다 세션 아이디 새로 발급 안함
+    saveUninitialized: true, //사용자가 접속해서 세션을 사용할 때까지 sid 발급 말 것
+    cookie:{secure: true}
+}))
+//join 쿼리 객체로 생성
 const join = {
     list: 'select * from register order by id desc',
     insert : 'insert into register(name, user_id, password, email, tel, reg_date) values(?, ?, ?, ?, ?, ?)',
-    read: 'select * from register where id=?'
+    read: 'select * from register where user_id=?'
 }
 
 var _storage = multer.diskStorage({
@@ -57,7 +67,7 @@ const date = moment().format('YYYY-MM-DD HH:mm:ss');
 
 //첫화면
 app.get('/', (req, res)=>{
-    res.render('index');
+    res.render('index', {'welcome':''});
 })
 
 //회원가입
@@ -76,7 +86,28 @@ app.post('/join', (req, res)=>{
         if(err) console.log(err);
         else{
             console.log('Inserted!');
-            res.redirect('/');
+            res.redirect('/login');
+        }
+    })
+})
+
+//로그인
+app.get('/login', (req, res)=>{
+    res.render('login', {'message':''});
+})
+
+app.post('/login', (req, res)=>{
+    const _id = req.body.id;
+    const _password = req.body.password;
+    conn.query(join.read, [_id], (err, user)=>{
+        if(err) console.log(err);
+        if(user[0].password === _password){
+            req.session.uid = req.body.id;
+            //TODO: 리스트 뿌려주기
+            res.render('index', {'welcome':`${req.session.uid}님 환영합니다.`});
+        }
+        else{
+            res.render('login', {'message':'아이디나 비밀번호가 올바르지 않습니다.'})
         }
     })
 })
